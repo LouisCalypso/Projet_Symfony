f8589159dd3e36ff7f1e68eb01e36b0be8d873da
Louis <louis.duretete@gmail.com>
2020-05-02 16:21:47 +0200

    Création BDD + Fixture + Classe : Post, User, Comment
    Avec Lucas
    Création de la BDD doctrine (ORM)
    Ajout des classes et des relations entre-elles (classe : USER, POST, COMMENT)
    Ajout de données FAKES dans la BDD avec Fixtures

---
+++ src/DataFixtures/PostFixtures.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Post;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use \DateTime;
+use Faker;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use App\DataFixtures\UserFixtures;
+
+class PostFixtures extends Fixture implements DependentFixtureInterface
+{
+    public const POST = 'post';
+    public const USER = 'user';
+
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create();
+        for ($i = 0; $i < 2; $i++) {
+
+            $post = new Post();
+            $post->setTitle($faker->text($maxNbChars = 10));
+            $post->setBody($faker->paragraph($nbSentences = 3));
+            $post->setCreatedAt($faker->dateTime($max = 'now', $timezone = null));
+            $post->setNbVotes($faker->randomDigit);
+            $this->addReference(self::POST.$i,$post);
+            $user = $this->getReference(UserFixtures::USER.$i);
+            $post->setUser($user);
+            $user->addPost($post);
+            $manager->persist($post);
+        }
+        for ($i = 2; $i < 4; $i++) {
+
+            $post = new Post();
+            $post->setTitle($faker->text($maxNbChars = 10));
+            $post->setImage($faker->imageUrl($width = 400, $height = 400));
+            $post->setCreatedAt($faker->dateTime($max = 'now', $timezone = null));
+            $post->setNbVotes($faker->randomDigit);
+            $this->addReference(self::POST.$i,$post);
+            $user = $this->getReference(UserFixtures::USER.$i);
+            $post->setUser($user);
+            $user->addPost($post);
+            $manager->persist($post);
+        }
+        $post = new Post();
+        $post->setTitle($faker->text($maxNbChars = 10));
+        $post->setLink("www.reddit.com");
+        $post->setCreatedAt($faker->dateTime($max = 'now', $timezone = null));
+        $post->setNbVotes($faker->randomDigit);
+        $this->addReference(self::POST.$i,$post);
+        $user = $this->getReference(UserFixtures::USER.$i);
+        $post->setUser($user);
+        $user->addPost($post);
+        $manager->persist($post);
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return array(
+            UserFixtures::class,
+        );
+    }
+}
